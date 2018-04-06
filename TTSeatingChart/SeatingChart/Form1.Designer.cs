4c181913d2e49c5529be36caab10dc29f8981df0
Trevor Terry <twiztedterry@gmail.com>
2018-04-06 16:15:53 -0600

    particpation 17

---
+++ TTSeatingChart/SeatingChart/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace SeatingChart
+{
+    partial class seatingChartForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.rowLabel = new System.Windows.Forms.Label();
+            this.columnLabel = new System.Windows.Forms.Label();
+            this.rowTextBox = new System.Windows.Forms.TextBox();
+            this.columnTextBox = new System.Windows.Forms.TextBox();
+            this.seatLabel = new System.Windows.Forms.Label();
+            this.priceLabel = new System.Windows.Forms.Label();
+            this.displayPriceButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.revenueButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::SeatingChart.Properties.Resources.Airplane;
+            this.pictureBox1.Location = new System.Drawing.Point(117, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(160, 238);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // rowLabel
+            // 
+            this.rowLabel.AutoSize = true;
+            this.rowLabel.Location = new System.Drawing.Point(12, 26);
+            this.rowLabel.Name = "rowLabel";
+            this.rowLabel.Size = new System.Drawing.Size(66, 13);
+            this.rowLabel.TabIndex = 1;
+            this.rowLabel.Text = "Enter a Row";
+            // 
+            // columnLabel
+            // 
+            this.columnLabel.AutoSize = true;
+            this.columnLabel.Location = new System.Drawing.Point(12, 100);
+            this.columnLabel.Name = "columnLabel";
+            this.columnLabel.Size = new System.Drawing.Size(79, 13);
+            this.columnLabel.TabIndex = 2;
+            this.columnLabel.Text = "Enter a Column";
+            // 
+            // rowTextBox
+            // 
+            this.rowTextBox.Location = new System.Drawing.Point(19, 42);
+            this.rowTextBox.Name = "rowTextBox";
+            this.rowTextBox.Size = new System.Drawing.Size(76, 20);
+            this.rowTextBox.TabIndex = 3;
+            // 
+            // columnTextBox
+            // 
+            this.columnTextBox.Location = new System.Drawing.Point(19, 116);
+            this.columnTextBox.Name = "columnTextBox";
+            this.columnTextBox.Size = new System.Drawing.Size(76, 20);
+            this.columnTextBox.TabIndex = 4;
+            // 
+            // seatLabel
+            // 
+            this.seatLabel.AutoSize = true;
+            this.seatLabel.Location = new System.Drawing.Point(283, 26);
+            this.seatLabel.Name = "seatLabel";
+            this.seatLabel.Size = new System.Drawing.Size(56, 13);
+            this.seatLabel.TabIndex = 5;
+            this.seatLabel.Text = "Seat Price";
+            // 
+            // priceLabel
+            // 
+            this.priceLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.priceLabel.Location = new System.Drawing.Point(296, 44);
+            this.priceLabel.Name = "priceLabel";
+            this.priceLabel.Size = new System.Drawing.Size(83, 23);
+            this.priceLabel.TabIndex = 6;
+            // 
+            // displayPriceButton
+            // 
+            this.displayPriceButton.Location = new System.Drawing.Point(19, 271);
+            this.displayPriceButton.Name = "displayPriceButton";
+            this.displayPriceButton.Size = new System.Drawing.Size(95, 23);
+            this.displayPriceButton.TabIndex = 7;
+            this.displayPriceButton.Text = "Display Price";
+            this.displayPriceButton.UseVisualStyleBackColor = true;
+            this.displayPriceButton.Click += new System.EventHandler(this.displayPriceButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(255, 271);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 8;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // revenueButton
+            // 
+            this.revenueButton.Location = new System.Drawing.Point(135, 271);
+            this.revenueButton.Name = "revenueButton";
+            this.revenueButton.Size = new System.Drawing.Size(91, 23);
+            this.revenueButton.TabIndex = 9;
+            this.revenueButton.Text = "Max Revenue";
+            this.revenueButton.UseVisualStyleBackColor = true;
+            this.revenueButton.Click += new System.EventHandler(this.revenueButton_Click);
+            // 
+            // seatingChartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(391, 320);
+            this.Controls.Add(this.revenueButton);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.displayPriceButton);
+            this.Controls.Add(this.priceLabel);
+            this.Controls.Add(this.seatLabel);
+            this.Controls.Add(this.columnTextBox);
+            this.Controls.Add(this.rowTextBox);
+            this.Controls.Add(this.columnLabel);
+            this.Controls.Add(this.rowLabel);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "seatingChartForm";
+            this.Text = "Seating Chart";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label rowLabel;
+        private System.Windows.Forms.Label columnLabel;
+        private System.Windows.Forms.TextBox rowTextBox;
+        private System.Windows.Forms.TextBox columnTextBox;
+        private System.Windows.Forms.Label seatLabel;
+        private System.Windows.Forms.Label priceLabel;
+        private System.Windows.Forms.Button displayPriceButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button revenueButton;
+    }
+}
+
---
+++ TTSeatingChart/SeatingChart/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SeatingChart
+{
+    public partial class seatingChartForm : Form
+    {
+        //adding the constants for row and column size
+        const int MAX_ROW = 5;
+        const int MAX_COL = 3;
+
+        //two dimensional array, initilized with price data
+        decimal[,] prices = {   {450m, 450m, 450m, 450m},
+                                       {425m, 425m, 425m, 425m},
+                                       {400m, 400m, 400m, 400m},
+                                       {375m, 375m, 375m, 375m},
+                                       {375m, 375m, 375m, 375m},
+                                       {350m, 350m, 350m, 350m}
+
+                                    };
+
+
+        public seatingChartForm()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// When clicked, closes the program
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            //closes the program
+            this.Close();
+        }
+        /// <summary>
+        /// When clicked, displays the price of the selected seats.
+        /// Verification will occur to make sure that proper entries are selected.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void displayPriceButton_Click(object sender, EventArgs e)
+        {
+            //local variables for row and column
+            int column, row;
+
+            //if statement logic to check the column text box, ensures the text in the box is an int
+            if (int.TryParse(columnTextBox.Text, out column))
+            {
+                //makes sure that the text in column is less than MAX_COL and greater or equal to 0
+                if (column >= 0 && column <=MAX_COL)
+                {
+                    //if statement to check the row text box, ensures the text is an int
+                    if (int.TryParse(rowTextBox.Text, out row))
+                    {
+                        //makes sure that the text is less than MAX_COL and greater or equal to 0
+                        if (row >= 0 && row <=MAX_ROW)
+                        {
+                            //sets the pricelabel text to the price value from the array
+                            priceLabel.Text = prices[row, column].ToString("c");
+                        }
+                        else
+                        {
+                            //sets the focus on the row box
+                            rowTextBox.Focus();
+                            //messagebox that explains row requirements.
+                            MessageBox.Show("Row entered must be between 0 and " + MAX_ROW);
+                        }
+                    }
+                    else
+                    {
+                        //sets the focus on the row box
+                        rowTextBox.Focus();
+                        //messagebox that explains row requirements.
+                        MessageBox.Show("Row entered must be a valid whole number");
+                    }
+                        
+                    
+
+                }
+                else
+                {
+                    //sets the focus on the column box
+                    columnTextBox.Focus();
+                    //messagebox that explains column requirements.
+                    MessageBox.Show("Column entered must be between 0 and " + MAX_COL);
+                }
+            }
+            else
+            {
+                //sets the focus on the column box
+                columnTextBox.Focus();
+                //messagebox that explaisn column requirements.
+                MessageBox.Show("Column entered must be a valid whole number");
+                    
+            }
+
+        }
+        /// <summary>
+        /// when clicked, uses a loop to sum all price values into the total. Outer loop for row and inner for column
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void revenueButton_Click(object sender, EventArgs e)
+        {
+            //local variable for total
+            decimal total = 0;
+
+            //nested for loop
+            //as long as our counter is less than the max row size
+            for (int i = 0; i< MAX_ROW; i++)
+            {
+                //and as long as another counter is less than the column size
+                for (int j = 0; j<MAX_COL; j++)
+                {
+                    //add the two together and add the total to the variable
+                    total += prices[i, j];
+                }
+            }
+
+            //displays the total in a messagebox
+            MessageBox.Show("The total revenue is :" + total.ToString("c"));
+
+        }
+    }
+}

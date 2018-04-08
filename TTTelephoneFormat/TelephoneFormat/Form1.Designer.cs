c8e26a71ecdee4507754fd80bb1d745c9e6b5142
Trevor Terry <twiztedterry@gmail.com>
2018-04-07 20:48:57 -0600

    Participation 18

---
+++ TTTelephoneFormat/TelephoneFormat/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace TelephoneFormat
+{
+    partial class telephoneFormatForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.instructionsLabel = new System.Windows.Forms.Label();
+            this.numberTextBox = new System.Windows.Forms.TextBox();
+            this.formattedLabel = new System.Windows.Forms.Label();
+            this.numberLabel = new System.Windows.Forms.Label();
+            this.formatButton = new System.Windows.Forms.Button();
+            this.instructions2Label = new System.Windows.Forms.Label();
+            this.telephoneTextBox = new System.Windows.Forms.TextBox();
+            this.telephoneLabel = new System.Windows.Forms.Label();
+            this.unformattedLabel = new System.Windows.Forms.Label();
+            this.unformatButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // instructionsLabel
+            // 
+            this.instructionsLabel.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.instructionsLabel.Location = new System.Drawing.Point(32, 19);
+            this.instructionsLabel.Name = "instructionsLabel";
+            this.instructionsLabel.Size = new System.Drawing.Size(221, 44);
+            this.instructionsLabel.TabIndex = 0;
+            this.instructionsLabel.Text = "Enter a 10-digit number and I will format it as a telephone number.";
+            this.instructionsLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // numberTextBox
+            // 
+            this.numberTextBox.Location = new System.Drawing.Point(53, 66);
+            this.numberTextBox.Name = "numberTextBox";
+            this.numberTextBox.Size = new System.Drawing.Size(167, 20);
+            this.numberTextBox.TabIndex = 1;
+            // 
+            // formattedLabel
+            // 
+            this.formattedLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.formattedLabel.Location = new System.Drawing.Point(53, 114);
+            this.formattedLabel.Name = "formattedLabel";
+            this.formattedLabel.Size = new System.Drawing.Size(167, 23);
+            this.formattedLabel.TabIndex = 2;
+            // 
+            // numberLabel
+            // 
+            this.numberLabel.AutoSize = true;
+            this.numberLabel.Location = new System.Drawing.Point(63, 92);
+            this.numberLabel.Name = "numberLabel";
+            this.numberLabel.Size = new System.Drawing.Size(148, 13);
+            this.numberLabel.TabIndex = 3;
+            this.numberLabel.Text = "Formatted Telephone Number";
+            // 
+            // formatButton
+            // 
+            this.formatButton.Location = new System.Drawing.Point(91, 152);
+            this.formatButton.Name = "formatButton";
+            this.formatButton.Size = new System.Drawing.Size(75, 23);
+            this.formatButton.TabIndex = 4;
+            this.formatButton.Text = "Format";
+            this.formatButton.UseVisualStyleBackColor = true;
+            this.formatButton.Click += new System.EventHandler(this.formatButton_Click);
+            // 
+            // instructions2Label
+            // 
+            this.instructions2Label.Location = new System.Drawing.Point(308, 25);
+            this.instructions2Label.Name = "instructions2Label";
+            this.instructions2Label.Size = new System.Drawing.Size(170, 33);
+            this.instructions2Label.TabIndex = 5;
+            this.instructions2Label.Text = "Enter a formatted telephone number and I will unformat it.";
+            this.instructions2Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // telephoneTextBox
+            // 
+            this.telephoneTextBox.Location = new System.Drawing.Point(308, 66);
+            this.telephoneTextBox.Name = "telephoneTextBox";
+            this.telephoneTextBox.Size = new System.Drawing.Size(157, 20);
+            this.telephoneTextBox.TabIndex = 6;
+            // 
+            // telephoneLabel
+            // 
+            this.telephoneLabel.AutoSize = true;
+            this.telephoneLabel.Location = new System.Drawing.Point(308, 92);
+            this.telephoneLabel.Name = "telephoneLabel";
+            this.telephoneLabel.Size = new System.Drawing.Size(159, 13);
+            this.telephoneLabel.TabIndex = 7;
+            this.telephoneLabel.Text = "Unformatted Telephone Number";
+            // 
+            // unformattedLabel
+            // 
+            this.unformattedLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.unformattedLabel.Location = new System.Drawing.Point(313, 114);
+            this.unformattedLabel.Name = "unformattedLabel";
+            this.unformattedLabel.Size = new System.Drawing.Size(154, 23);
+            this.unformattedLabel.TabIndex = 8;
+            // 
+            // unformatButton
+            // 
+            this.unformatButton.Location = new System.Drawing.Point(362, 152);
+            this.unformatButton.Name = "unformatButton";
+            this.unformatButton.Size = new System.Drawing.Size(75, 23);
+            this.unformatButton.TabIndex = 9;
+            this.unformatButton.Text = "Unformat";
+            this.unformatButton.UseVisualStyleBackColor = true;
+            this.unformatButton.Click += new System.EventHandler(this.unformatButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(178, 214);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 10;
+            this.clearButton.Text = "Clear All";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(308, 214);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 11;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // telephoneFormatForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 257);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.unformatButton);
+            this.Controls.Add(this.unformattedLabel);
+            this.Controls.Add(this.telephoneLabel);
+            this.Controls.Add(this.telephoneTextBox);
+            this.Controls.Add(this.instructions2Label);
+            this.Controls.Add(this.formatButton);
+            this.Controls.Add(this.numberLabel);
+            this.Controls.Add(this.formattedLabel);
+            this.Controls.Add(this.numberTextBox);
+            this.Controls.Add(this.instructionsLabel);
+            this.Name = "telephoneFormatForm";
+            this.Text = "Telephone Format/Unformat";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label instructionsLabel;
+        private System.Windows.Forms.TextBox numberTextBox;
+        private System.Windows.Forms.Label formattedLabel;
+        private System.Windows.Forms.Label numberLabel;
+        private System.Windows.Forms.Button formatButton;
+        private System.Windows.Forms.Label instructions2Label;
+        private System.Windows.Forms.TextBox telephoneTextBox;
+        private System.Windows.Forms.Label telephoneLabel;
+        private System.Windows.Forms.Label unformattedLabel;
+        private System.Windows.Forms.Button unformatButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
---
+++ TTTelephoneFormat/TelephoneFormat/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TelephoneFormat
+{
+    public partial class telephoneFormatForm : Form
+    {
+        //constants
+        const int VALID_LENGTH = 10;
+        const int UNFORMAT_VALID_LENGTH = 13;
+
+
+        public telephoneFormatForm()
+        {
+            InitializeComponent();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            numberTextBox.Text = "";
+            telephoneTextBox.Text = "";
+            formattedLabel.Text = "";
+            unformattedLabel.Text = "";
+        }
+        /// <summary>
+        /// Event handler for the format button, when pressed, formats the phone number into the correct format, after checking to ensure the phone number isn't already properly formatted.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void formatButton_Click(object sender, EventArgs e)
+        {
+
+            //creates the variable and assigns the textbox, while trimming all empty spaces.
+            string str = numberTextBox.Text.Trim();
+
+            //if statement that uses the validnumber method to check the validity of the number, then performs actions if its valid, displays an error otherwise.
+            if(ValidNumber(str))
+            {
+                //runs the telephoneformat method, formatting the number
+                TelephoneFormat(ref str);
+                //assigns the formatted number to the label's text
+                formattedLabel.Text = str;
+            }
+            //displays a relevant error message
+            else
+            {
+                //displays message
+                MessageBox.Show("The phone number entered is not valid.");
+            }
+          
+        }
+        /// <summary>
+        /// method to do validation checking on the entered text, returns true if valid format, false otherwise.
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        private bool ValidNumber(string str)
+        {
+            //tests the string to see if it's equal to valid length constant
+            if(str.Length == VALID_LENGTH)
+            {
+                //foreach loop checking to see if each character is a digit, returns false otherwise.
+                foreach(char ch in str)
+                {
+                    //if statement within the foreach loop to verify each character is a digit
+                   if(!char.IsDigit(ch))
+                    {
+                        //returns false if one of them isn't a digit
+                        return false;
+                    }
+                }
+                //returns true
+                return true;
+            }
+
+            //if it's not
+            else
+            {
+                //return a false value to the bool
+                return false;
+            }
+
+        }
+        /// <summary>
+        /// method to insert the characters into the number to make it into a valid phone number
+        /// </summary>
+        /// <param name="str"></param>
+        private void TelephoneFormat(ref string str)
+        {
+            //inserts a left open parentheses at location zero
+            str = str.Insert(0, "(");
+            //inserts a right closed parentheses at location three
+            str = str.Insert(4, ")");
+            //inserts a dash at location seven.
+            str = str.Insert(8, "-");
+        }
+        /// <summary>
+        /// Method to test the format for the unformat button.
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        private bool ValidFormat(string str)
+        {
+            //checks to see if the length is valid, and that there's an open parentehses at 0, a close at 3, and a dash at 7
+            if (str.Length == UNFORMAT_VALID_LENGTH && str[0] == '(' && str[4] == ')' && str[8] == '-')
+            {
+                //returns true
+                return true;
+            }
+            else
+            {
+                //returns false
+                return false;
+            }   
+        }
+
+        /// <summary>
+        /// method that removes the formatting from the entered telephone number
+        /// </summary>
+        /// <param name="str"></param>
+
+        private void UnformatTelephone(ref string str)
+        {
+            //removes the character at the zero position
+            str = str.Remove(0, 1);
+            //removes the character at the four position
+            str = str.Remove(3, 1);
+            //removes the character at the seven position
+            str = str.Remove(6, 1);
+        }
+
+        /// <summary>
+        /// event handler for the unformat button, when pressed, will remove formatting from the entered phone number.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void unformatButton_Click(object sender, EventArgs e)
+        {
+            //grabs the textbox text and stores it in a string variable after trimming
+            string str = telephoneTextBox.Text.Trim();
+
+            //uses the established method to check to see if the format is already valid, if not - formats it properly
+            if(ValidFormat(str))
+            {
+                //runs the unformattelephone method, which removes the formatting from a telepehone nubmer entered.
+                UnformatTelephone(ref str);
+                //changes the label text to be equal to the newly unformatted phone number.
+                unformattedLabel.Text = str;
+
+            }
+            else
+            {
+                //shows a messagebox with proper text
+                MessageBox.Show("The telephone number entered is not valid");
+            }
+
+
+        }
+
+       
+    }
+}
